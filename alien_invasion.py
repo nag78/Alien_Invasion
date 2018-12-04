734909f9c0a40545308a46ffb0c57ee4d4d380ff
Nikolay Goltsev <nag78@yandex.ru>
2018-12-04 13:15:38 +0300

    class Settings
    Выносим настройки в отдельный класс

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,17 @@
 import sys
 
 import pygame
+from settings import Settings
 
 def run_game():
     #Инициализируем игру и создаем объект экрана.
+
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    #Назначение цвета фона
-    bg_color = (230,230,230)
+
 
     #Запуск основного цикла игры
     while True:
@@ -17,7 +20,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
         #При каждом проходе цикла перерисовывается экрае
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
         #Отображение последнего прорисованного экрана.
         pygame.display.flip()
 run_game()
