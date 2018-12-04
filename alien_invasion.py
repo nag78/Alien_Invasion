26e8caa03c93b59f87c3839fd3c39dd21f41bc8d
Nikolay Goltsev <nag78@yandex.ru>
2018-12-04 13:03:35 +0300

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,8 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
+    #Назначение цвета фона
+    bg_color = (230,230,230)
 
     #Запуск основного цикла игры
     while True:
@@ -14,7 +16,8 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
+        #При каждом проходе цикла перерисовывается экрае
+        screen.fill(bg_color)
         #Отображение последнего прорисованного экрана.
         pygame.display.flip()
 run_game()
