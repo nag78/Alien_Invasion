7702ebd12cd99d70b36526d81b8299d6e53902d2
Nikolay Goltsev <nag78@yandex.ru>
2018-12-11 18:40:59 +0300

    Up

--- game_functions.py
+++ game_functions.py
@@ -59,7 +59,7 @@ def check_keyup_event(event,ai_settings,screen,ship,bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_settings,screen,ship,bullets):
+def check_events(ai_settings,screen,stats,play_button,ship,bullets):
     """
     Обрабатывает нажатия клавиш и события мыши.
     """
@@ -72,6 +72,17 @@ def check_events(ai_settings,screen,ship,bullets):
         #Клавиша отпущена перемещение остановлено
         elif event.type == pygame.KEYUP:
             check_keyup_event(event,ai_settings,screen,ship,bullets)
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(stats,play_button,mouse_x,mouse_y)
+
+def check_play_button(stats,play_button,mouse_x,mouse_y):
+    """
+    Запускает новую игру при нажатии кнопки Play.
+    """
+    if play_button.rect.collidepoint(mouse_x,mouse_y):
+        stats.game_active = True
 
 def update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button):
     """
