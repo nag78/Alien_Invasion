1fdb05622e78b1ecf9b3b69bfed3c75f55884a02
Nikolay Goltsev <nag78@yandex.ru>
2018-12-26 17:37:39 +0300

    Up

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,6 @@
 import pygame.font
+from pygame.sprite import Group
+from ship import Ship
 
 
 class Scoreboard():
@@ -17,6 +19,7 @@ class Scoreboard():
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
+        self.prep_ships()
 
     def prep_score(self):
         """Преобразует текущий счет в графическое изображения"""
@@ -34,6 +37,8 @@ class Scoreboard():
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        # Вывод кораблей
+        self.ships.draw(self.screen)
 
     def prep_high_score(self):
         """Преобразует рекордный счет в графическое изображениe."""
@@ -57,3 +62,13 @@ class Scoreboard():
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
+
+    def prep_ships(self):
+        """Сообщает количество оставшихся кораблей.
+        """
+        self.ships = Group()
+        for ship_number in range(self.stats.ship_left):
+            ship = Ship(self.ai_settings, self.screen)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
